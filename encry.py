3933def932b947793e0b4af8ac10674690c7cfe8
Muhammed Enes gürkan <90131331+koftee@users.noreply.github.com>
2024-05-07 16:24:34 +0300

    Update encry.py

--- encry.py
+++ encry.py
@@ -1,1 +1,15 @@
 
+from cryptography.fernet import Fernet
+
+# Anahtar oluştur
+key = Fernet.generate_key()
+cipher = Fernet(key)
+
+# Şifrelenecek mesaj
+username_token =cipher.encrypt(b"okul numaranız")
+password_token = cipher.encrypt(b"şifreniz")
+# Mesajı şifrele
+decrypted_username = cipher.decrypt(username_token)
+str_decrypted_usernmae = decrypted_username.decode()
+decrypted_pasword= cipher.decrypt(password_token)
+str_decrypted_password = decrypted_pasword.decode()
